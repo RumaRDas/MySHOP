57da0d1ac236574e0d6b5f99ac0aaf0037f69bab
Ruma Das <rumaadl16@gmail.com>
2022-12-24 00:44:46 +1030

    adding home screen and product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './Screens/HomeScreen';
 const App=() =>{
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1> Welcome to Myshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
