e09f8e057bcae071973cb281bf38fd22ded55534
Ruma Das <rumaadl16@gmail.com>
2022-12-28 22:35:42 +1030

    getting products to frontend from mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 
 
 dotenv.config()
@@ -12,14 +13,13 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('API is Running........')
 })
+app.use('/api/products', productRoutes)
+//For handling error 404  getting json responces in development mode
+app.use(notFound)
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+//For handling error 500  getting json responces in development mode
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
